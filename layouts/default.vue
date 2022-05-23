f125e2ec08b6090b312e3cb02230658f20f60838
Francesco <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2022-05-23 16:16:14 +0200

    footer almost finished

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,18 @@
     />
     <the-header />
     <nuxt />
+    <the-footer />
   </div>
 </template>
 
 <script>
+import TheFooter from '~/components/TheFooter.vue'
 import TheHeader from '~/components/TheHeader.vue'
 export default {
   name: 'DefaultLayout',
   components: {
     TheHeader,
+    TheFooter,
   },
 }
 </script>
