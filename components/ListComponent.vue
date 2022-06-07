9893b356ddcda553b76cd99d872534aa0c58a2d5
FBosso <francesco.bosso@mail.polimi.it>
2022-06-07 08:31:16 +0200

    services title new style

--- components/ListComponent.vue
+++ components/ListComponent.vue
@@ -17,10 +17,10 @@ The required parameters are:
   <!-- the clas mb-4 provide a margin-bottom of 4 px in order to allow mobile user to visually 
   percieve when a service ends and the next one starts (on desktop this problem is not evident 
   because the services are organized in rows and it is more easy to distiguish them) -->
-  <div class="row mb-4">
+  <div class="row mb-5">
     <!-- the class column will create the space for the image of the serice (displayed thanks 
     to the StaticCardComponent) -->
-    <div class="col-md-5">
+    <div class="col-md-5 order-xl-1 order-2 ">
       <!-- here the StaticCardComponent is passed inside this component in order to display 
       the image of the service -->
       <StaticCardComponent :image="image" :height="height" />
@@ -28,25 +28,31 @@ The required parameters are:
     <!-- this second column will contain two elements:
       - the opening hours of the service
       - the general info of the service (address, site/name, rating) -->
-    <div class="col-md-7">
-      <div class="row">
+
+    <!-- <div class="col-md-7">
+      <div class="row"> -->
+
         <!-- here is the column of the opening hours -->
-        <div class="col-xl-7">
+        <div class="col-xl-4 order-xl-2 order-3">
           <!-- here the OpeningHoursComponent is passed inside this component in order to display 
           the opening hours block (the creation of a specific component was needed because it is 
           also used in the TopMapDescription component ) -->
           <OpeningHoursComponent :hours="hours" />
         </div>
         <!-- here is the columns of the general info -->
-        <div class="col">
-          <div class="info-style">
+        <div class="col-xl-3 order-xl-3 order-1">
+          <div class="info-style ">
+            <h1 class="p-1">{{ name }}</h1>
+            <hr style="margin-top:0;">
             <p><span class="attribute">Address: </span>{{ address }}</p>
-            <p><span class="attribute">Site: </span><a :href="`${website}`">{{ name }}</a></p>
+            <p><span class="attribute">Site: </span><a :href="`${website}`">{{ name }}'s website</a></p>
             <p><span class="attribute">Rating: </span>{{ rating }}/5</p>
           </div>
         </div>
-      </div>
-    </div>
+
+     <!--  </div>
+    </div> -->
+
   </div>
 </template>
 
@@ -98,5 +104,8 @@ export default {
 }
 .attribute {
   font-weight: 600;
+}
+h1 {
+  font-size: 180%;
 }
 </style>
